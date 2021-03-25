982a5f40fc120fbcde7aca4fd7f7b7369567f224
Gabriel Ferreira <gabriel.ferreira.itba@gmail.com>
2021-03-25 16:32:41 -0300

    refactor: organização do setMasks

--- src/utils/setMasks.ts
+++ src/utils/setMasks.ts
@@ -1,53 +1,60 @@
-export const cpfMask = (value: string): string =>
-  value
+export const cpfMask = (value: string): string => {
+  return value
     .replace(/[^\d]/g, '')
     .replace(/(\d{3})(\d)/, '$1.$2')
     .replace(/(\d{3})(\d)/, '$1.$2')
     .replace(/(\d{3})(\d{1,2})/, '$1-$2')
     .replace(/(-\d{2})\d+?$/, '$1');
+};
 
-export const rgMask = (value: string): string =>
-  value
+export const rgMask = (value: string): string => {
+  return value
     .replace(/[^\d]/g, '')
     .replace(/(\d{2})(\d)/, '$1.$2')
     .replace(/(\d{3})(\d)/, '$1.$2')
     .replace(/(\d{3})(\d{1,2})/, '$1-$2')
     .replace(/(-\d{1})\d+?$/, '$1');
+};
 
-export const cnpjMask = (value: string): string =>
-  value
+export const cnpjMask = (value: string): string => {
+  return value
     .replace(/[^\d]/g, '')
     .replace(/(\d{2})(\d)/, '$1.$2')
     .replace(/(\d{3})(\d)/, '$1.$2')
     .replace(/(\d{3})(\d{1,2})/, '$1/$2')
     .replace(/(\d{4})(\d{1,2})/, '$1-$2')
     .replace(/(-\d{2)\d+?$/, '$1');
+};
 
-export const ieMask = (value: string): string =>
-  value
+export const ieMask = (value: string): string => {
+  return value
     .replace(/[^\d]/g, '')
     .replace(/(\d{2})(\d)/, '$1.$2')
     .replace(/(\d{3})(\d)/, '$1.$2')
     .replace(/(\d{2})(\d{2})/, '$1-$2')
     .replace(/(-\d{1})\d+?$/, '$1');
+};
 
-export const dateMask = (value: string): string =>
-  value
+export const dateMask = (value: string): string => {
+  return value
     .replace(/[^\d]/g, '')
     .replace(/(\d{2})(\d)/, '$1/$2')
     .replace(/(\d{2})(\d)/, '$1/$2')
     .replace(/(-\d{4})\d+?$/, '$1');
+};
 
-export const phoneMask = (value: string): string =>
-  value
+export const phoneMask = (value: string): string => {
+  return value
     .replace(/[^\d]/g, '')
     .replace(/(\d{2})(\d)/, '($1) $2')
     .replace(/(\d{4})(\d)/, '$1-$2')
     .replace(/(-\d{4})\d+?$/, '$1');
+};
 
-export const cellPhoneMask = (value: string): string =>
-  value
+export const cellPhoneMask = (value: string): string => {
+  return value
     .replace(/[^\d]/g, '')
     .replace(/(\d{2})(\d)/, '($1) $2')
     .replace(/(\d{5})(\d)/, '$1-$2')
     .replace(/(-\d{4})\d+?$/, '$1');
+};
